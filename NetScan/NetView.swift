6590070bbc182e1b2cb6c7ac9f8e85c53126b91f
Martin Martinez <mtz.martin@proton.me>
2023-12-19 15:35:41 -0600

    I will use this view for all my components so the content view is not overboard with it

---
+++ NetScan/NetView.swift
@@ -0,0 +1,18 @@
+//
+//  NetView.swift
+//  NetScan
+//
+//  Created by Martin Martinez on 19/12/23.
+//
+
+import SwiftUI
+
+struct NetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetView()
+}
