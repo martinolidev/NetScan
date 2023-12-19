e407598174deb535296706c3070098c50a49fb22
Martin Martinez <mtz.martin@proton.me>
2023-12-19 15:46:21 -0600

    Port form

--- NetScan/NetView.swift
+++ NetScan/NetView.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct NetView: View {
     @State var ip: String = ""
+    @State var ports: String = ""
     
     var body: some View {
         Text("NetScan")
             .font(.largeTitle)
             .bold()
         
+        Divider()
+            .padding(.bottom)
+        
         HStack {
             Text("IP address")
             TextField("IP address to scan", text: $ip)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+        }
+        
+        HStack(spacing: 48){
+            Text("Ports")
+                .frame(alignment: .leading)
+            TextField("All ports by the default", text: $ports)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
         }
