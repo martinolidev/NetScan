817cd40ccf029b5450d9abdf7004e90b5b0286fc
Martin Martinez <mtz.martin@proton.me>
2023-12-19 15:54:19 -0600

    View for the results of the scan

---
+++ NetScan/ResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsView.swift
+//  NetScan
+//
+//  Created by Martin Martinez on 19/12/23.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultsView()
+}
