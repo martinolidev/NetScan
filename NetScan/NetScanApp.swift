f941c809ae4d741b612d3bfbc4ed16f9dd1372b0
Martin Martinez <mtz.martin@proton.me>
2023-12-19 15:07:44 -0600

    Initial Commit

---
+++ NetScan/NetScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetScanApp.swift
+//  NetScan
+//
+//  Created by Martin Martinez on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NetScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
