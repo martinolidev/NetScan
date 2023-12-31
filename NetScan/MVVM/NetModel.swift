00c21c6bf3442f8dbf9105eed43d30e830f4ff6d
Martin Martinez <mtz.martin@proton.me>
2023-12-31 17:11:01 -0600

    My request method is ready, I forgot to set up my server with the API, I'll back :D

--- NetScan/MVVM/NetModel.swift
+++ NetScan/MVVM/NetModel.swift
@@ -6,17 +6,34 @@
 //
 
 import Foundation
+import Alamofire
 
-struct portResponse: Codable {
-    var ports = [String]()
+struct portContent: Codable {
+    var ports: [String]
 }
 
 class NetModel {
     
-    var portInformation: portResponse
-        
+    var portInformation: portContent
+    //Change this IP to your server IP with my API
+    var url: String = "http://127.0.0.1:8080"
+    
     init () {
-        portInformation = portResponse(ports: ["8080"])
+        portInformation = portContent(ports: ["8080"])
+    }
+    
+    func sendInformation() -> portContent {
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: portContent.self) {
+            response in
+            
+            if let content = response.value {
+                self.portInformation = portContent(ports: content.ports)
+            } else {
+                print(response.error?.responseCode ?? "No code error")
+            }
+        }
+        
+        return portInformation
     }
     
 }
