43dda1e9ad17e441e86f5211fc255b1e1fa63579
Martin Martinez <mtz.martin@proton.me>
2023-12-31 16:52:22 -0600

    Model

--- NetScan/MVVM/NetModel.swift
+++ NetScan/MVVM/NetModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct portResponse: Codable {
+    var ports = [String]()
+}
+
+class NetModel {
+    
+    var portInformation: portResponse
+        
+    init () {
+        portInformation = portResponse(ports: ["8080"])
+    }
+    
+}
