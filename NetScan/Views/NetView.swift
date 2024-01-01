e498f52bf2dd547fa24929a23ed995cefcbaf1c7
Martin Martinez <mtz.martin@proton.me>
2023-12-31 22:33:22 -0600

    I get the IP address from the textfield

--- NetScan/Views/NetView.swift
+++ NetScan/Views/NetView.swift
@@ -45,7 +45,7 @@ struct NetView: View {
                 )
         }
         
-        ResultsView(openPorts: ["22", "80", "443"], ipToScan: "192.168.1.1")
+        ResultsView(openPorts: ["22", "80", "443"], ipToScan: ip)
     }
 }
 
