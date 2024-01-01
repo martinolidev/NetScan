40a91d9e4be84ddd0afb23e7da451dc217c7a04b
Martin Martinez <mtz.martin@proton.me>
2023-12-31 22:22:04 -0600

    Creating my view model

--- NetScan/MVVM/NetViewModel.swift
+++ NetScan/MVVM/NetViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class NetViewModel: ObservableObject {
+    @Published var response: portContent
+    
+    init() {
+        response = portContent(portNumbers: ["8080"])
+    }
+    
+    let model = NetModel()
+    
+    func makeRequest() {
+        response = model.sendInformation(ip: "192.168.1.147")
+    }
+}
